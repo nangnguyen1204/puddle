f0c2fdfa476042010fff1b60034e640ad89a78ed
unknown <congnang1242000@gmail.com>
2023-10-24 23:29:37 +0700

    update 24/10

--- puddle/puddle/item/views.py
+++ puddle/puddle/item/views.py
@@ -1,15 +1,28 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 
 from .forms import NewItemForm, EditItemForm
-from .models import Item
+from .models import Item, Category
 
 
 def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category',0)
+    categories = Category.objects.all()
     items = Item.objects.filter(is_sold = False)
+
+    if category_id:
+        items = items.filter(category_id = category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains = query) | Q(description__icontrains = query))
 
     return render(request, 'item/items.html',{
         'items': items,
+        'categories': categories,
+        'query': query,
+        'category_id': int(category_id)
     })
 
 def detail(request, pk):
@@ -18,7 +31,7 @@ def detail(request, pk):
 
     return render(request, 'item/detail.html', {
         'item': item,
-        'related_items': related_items
+        'related_items': related_items,
     })
 
 @login_required
--- puddle/puddle/puddle/urls.py
+++ puddle/puddle/puddle/urls.py
@@ -10,6 +10,7 @@ from core.views import index, contact
 urlpatterns = [
     path('', include('core.urls')),
     path('item/', include('item.urls')),
+    path('inbox', include('conversation.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
